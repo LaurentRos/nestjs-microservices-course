45f4b8c3409849502957003fe09c94d56ba14a08
Laurent Ros <laurent@mediflash.fr>
2024-08-12 10:09:05 +0200

    :construction: Setup NATS communication

--- apps/virtual-facility/src/buildings/buildings.module.ts
+++ apps/virtual-facility/src/buildings/buildings.module.ts
@@ -3,9 +3,22 @@ import { BuildingsService } from './buildings.service';
 import { BuildingsController } from './buildings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Building } from 'apps/virtual-facility/src/buildings/entities/building.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { WORKFLOWS_SERVICE } from 'apps/virtual-facility/src/constants';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Building])],
+  imports: [
+    TypeOrmModule.forFeature([Building]),
+    ClientsModule.register([
+      {
+        name: WORKFLOWS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: process.env.NATS_URL,
+        },
+      },
+    ]),
+  ],
   controllers: [BuildingsController],
   providers: [BuildingsService],
 })
