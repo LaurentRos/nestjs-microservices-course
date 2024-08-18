317cc16aea7acb70a9b7aab501697aa38ff9146e
Laurent Ros <laurent@mediflash.fr>
2024-08-18 16:30:51 +0200

    :construction: Setup inbox pattern

--- apps/workflows-service/src/workflows/workflows.controller.ts
+++ apps/workflows-service/src/workflows/workflows.controller.ts
@@ -1,15 +1,42 @@
 import { CreateWorkflowDto, UpdateWorkflowDto } from '@app/workflows';
 import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { WorkflowsService } from './workflows.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Inbox } from 'apps/workflows-service/src/inbox/entities/inbox.entity';
+import { Repository } from 'typeorm';
 
 @Controller('workflows')
 export class WorkflowsController {
-  constructor(private readonly workflowsService: WorkflowsService) {}
+  constructor(
+    private readonly workflowsService: WorkflowsService,
+    @InjectRepository(Inbox)
+    private readonly inboxRepository: Repository<Inbox>,
+  ) {}
 
   @EventPattern('workflows.create')
-  create(@Payload() createWorkflowDto: CreateWorkflowDto) {
-    return this.workflowsService.create(createWorkflowDto);
+  async create(
+    @Payload() createWorkflowDto: CreateWorkflowDto,
+    @Ctx() context: RmqContext,
+  ) {
+    const message = context.getMessage();
+    const inboxMessage = await this.inboxRepository.findOne({
+      where: {
+        messageId: message.properties.messageId,
+      },
+    });
+
+    if (!inboxMessage) {
+      await this.inboxRepository.save({
+        messageId: message.properties.messageId,
+        pattern: context.getPattern(),
+        status: 'pending',
+        payload: createWorkflowDto,
+      });
+    }
+
+    const channel = context.getChannelRef();
+    channel.ack(message);
   }
 
   @Get()
